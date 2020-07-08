61bed663bc3113f375e8e4a96adafb64ef54789d
Carlos Andrés Sierra Betancur <carlossierra311@users.noreply.github.com>
2020-07-08 15:59:28 -0500

    Solve mini-project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+import sys
+from time import time
+from sklearn.naive_bayes import GaussianNB
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
-import sys
-from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# create, train and use the classifier to make predictions on
+classifier = GaussianNB()
+# fit the model and time it
+t0 = time()
+classifier.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+# make predictions for the test data, and time it
+t0 = time()
+predictions = classifier.predict(features_test)
+print "testing time:", round(time()-t0, 3), "s"
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+# calculate and print the accuracy of the model
+accuracy = classifier.score(features_test, labels_test)
+print('Model accuracy is: {}'.format(accuracy))
