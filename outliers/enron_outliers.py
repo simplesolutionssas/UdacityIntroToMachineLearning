502d06c10824dc3a55cb024e39569c7b29f1360e
Carlos Sierra <carlossierra311@users.noreply.github.com>
2020-08-14 19:24:24 -0500

    Solve lesson 8 problems The modified files were enron_outliers.py, outlier_cleaner.py and outlier_removal_regression.py

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -3,17 +3,34 @@
 import pickle
 import sys
 import matplotlib.pyplot
+from pandas import DataFrame
+import numpy as np
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
-### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-features = ["salary", "bonus"]
-data = featureFormat(data_dict, features)
+# read in data dictionary, convert to numpy array
+file_path = '../final_project/final_project_dataset.pkl'
+file = open(file_path, 'r')
+enron_data = pickle.load(file)
+file.close()
+features = ['salary', 'bonus']
 
+# identify and remove the outliers that don't belong to the dataset
+enron_df = DataFrame.from_dict(enron_data, orient='index')
+enron_df.replace('NaN', np.nan, regex=True, inplace=True)
+enron_df.sort_values('salary', ascending=False).head()
+enron_data.pop('TOTAL', 0)
 
-### your code below
+# format the features for plot use
+data = featureFormat(enron_data, features)
 
+# visualize the data
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter(salary, bonus)
 
-
+matplotlib.pyplot.xlabel('salary')
+matplotlib.pyplot.ylabel('bonus')
+matplotlib.pyplot.show()
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import numpy as np
+
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -7,14 +9,23 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
     cleaned_data = []
+    # your code goes here
+    errors = abs(net_worths - predictions)
+    points_to_clean = int(len(predictions) * 0.1)
+    points_to_keep = len(predictions) - points_to_clean
+    # this function returns an unordered array of indices where the point at
+    # the points_to_keep position is in the order it would be if the array
+    # was ordered, and all elements before it are smaller than that, and all
+    # elements that are bigger come after it (although the particular order on
+    # the two partitions isn't guaranteed). we then slice this array, getting
+    # only the first points_to_keep, which are the array's smaller elements.
+    indices_to_keep = np.argpartition(errors, points_to_keep,
+                                      axis=0)[:points_to_keep, 0]
+    cleaned_data = [(ages[index], net_worths[index], errors[index])
+                    for index in indices_to_keep]
 
-    ### your code goes here
-
-    
     return cleaned_data
-
