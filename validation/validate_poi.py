e5b58b7be1f75c00a89671bcef0c483d2b646900
Carlos Sierra <carlossierra311@users.noreply.github.com>
2020-09-21 22:48:40 -0500

    Solve the rest of lesson 14 problems
    The modified file was validate_poi.py.

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,21 +12,35 @@
 
 import pickle
 import sys
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
+# load data from file
+file = '../final_project/final_project_dataset.pkl'
+data_dict = pickle.load(open(file, 'r'))
+
+# first element is our labels, any added elements are predictor
+# features. Keep this the same for the mini-project, but you'll
+# have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
-
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+# first classification attempt, without testing data.
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)
+score = clf.score(features, labels)
+print('initial decision tree score: {}'.format(score))
 
+# split data for training and testing
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
 
-### it's all yours from here forward!  
-
-
+# second classification attempt, but with training data this time.
+improved_clf = DecisionTreeClassifier()
+improved_clf.fit(features_train, labels_train)
+score = improved_clf.score(features_test, labels_test)
+print('validated decision tree score: {}'.format(score))
