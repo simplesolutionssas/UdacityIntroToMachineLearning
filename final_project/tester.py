eb8c510ba97a4763e7a3ec155b8de28956066de8
Carlos Sierra <carlossierra311@users.noreply.github.com>
2020-10-04 10:45:09 -0500

    Fix some formatting issues in tester.py

--- final_project/tester.py
+++ final_project/tester.py
@@ -1,8 +1,8 @@
 #!/usr/bin/pickle
 
 """ a basic script for importing student's POI identifier,
-    and checking the results that they get from it 
- 
+    and checking the results that they get from it
+
     requires that the algorithm, dataset, and features list
     be written to my_classifier.pkl, my_dataset.pkl, and
     my_feature_list.pkl, respectively
@@ -17,27 +17,30 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
-PERF_FORMAT_STRING = "\tAccuracy: {:>0.{display_precision}f}\
-                      \tPrecision: {:>0.{display_precision}f}\
-                      \tRecall: {:>0.{display_precision}f}\
-                      \tF1: {:>0.{display_precision}f}\
-                      \tF2: {:>0.{display_precision}f}"
-RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\
-                         \tTrue positives: {:4d}\
-                         \tFalse positives: {:4d}\
-                         \tFalse negatives: {:4d}\
-                         \tTrue negatives: {:4d}"
+CLF_PICKLE_FILENAME = "my_classifier.pkl"
+DATASET_PICKLE_FILENAME = "my_dataset.pkl"
+FEATURE_LIST_FILENAME = "my_feature_list.pkl"
+PERF_FORMAT_STRING = "\nAccuracy: {:>0.{display_precision}f}\
+                      \nPrecision: {:>0.{display_precision}f}\
+                      \nRecall: {:>0.{display_precision}f}\
+                      \nF1: {:>0.{display_precision}f}\
+                      \nF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "\nTotal predictions: {:4d}\
+                         \nTrue positives: {:4d}\
+                         \nFalse positives: {:4d}\
+                         \nFalse negatives: {:4d}\
+                         \nTrue negatives: {:4d}"
 
 
 def test_classifier(clf, dataset, feature_list, folds=1000):
     data = featureFormat(dataset, feature_list, sort_keys=True)
     labels, features = targetFeatureSplit(data)
-    cv = StratifiedShuffleSplit(labels, folds, random_state=42)
+    cv = StratifiedShuffleSplit(n_splits=folds, random_state=42)
     true_negatives = 0
     false_negatives = 0
     true_positives = 0
     false_positives = 0
-    for train_idx, test_idx in cv:
+    for train_idx, test_idx in cv.split(features, labels):
         features_train = []
         features_test = []
         labels_train = []
@@ -85,11 +88,6 @@ def test_classifier(clf, dataset, feature_list, folds=1000):
         print("Got a divide by zero when trying out:", clf)
         print("Precision or recall may be undefined due to a lack of true "
               "positive predicitons.")
-
-
-CLF_PICKLE_FILENAME = "my_classifier.pkl"
-DATASET_PICKLE_FILENAME = "my_dataset.pkl"
-FEATURE_LIST_FILENAME = "my_feature_list.pkl"
 
 
 def dump_classifier_and_data(clf, dataset, feature_list):
